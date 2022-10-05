448a186d3c2cf9ed47f50238a340643f8c39383e
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-05 18:16:11 -0500

    Fundamentos de C# | Trabajando con DateTime en C#

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -6,37 +6,19 @@ namespace gavilanch2_Programando_en_CSharp
     {
         static void Main(string[] args)
         {
-            var a = 1;
-            var b = 2;
-            var c = 8;
-            var d = 9;
+            DateTime fecha = new DateTime(2014, 10, 23);        
+            DateTime fechaConHora = new DateTime(2005, 4, 23, 9, 30, 45);
 
-            Console.WriteLine("Operaciones Logicas");
+            Console.WriteLine(fecha.ToString());
+            Console.WriteLine(fecha.ToString("MM/dd/yyyy"));
+            Console.WriteLine(fecha.ToString("dd-MM-yy"));
+            Console.WriteLine(fechaConHora.ToString("dd*MM*yy hh:mm:ss"));
+            Console.WriteLine(fecha.AddDays(45).ToString());
 
-            var booleano = true;
-            Console.WriteLine("Operacion negacion: !{0} = {1}", booleano, !booleano);
-            Console.WriteLine("");
+            Console.WriteLine(fechaConHora.DayOfWeek);
+            Console.WriteLine(fechaConHora.Date);
 
-            Console.WriteLine("Conjuncion");
-            Console.WriteLine("{0} & {0} = {1}", true, true & true);
-            Console.WriteLine("{0} & {0} = {1}", false, false & false);
-            Console.WriteLine("{0} & {1} = {2}", true, false, true & false);
-
-            //&&: El segundo operando solo se evalua si el primero es true
-
-            Console.WriteLine("");
-
-            Console.WriteLine("Disyuncion exclusiva");
-            Console.WriteLine("{0} ^ {0} = {1}", true, true ^ true);
-            Console.WriteLine("{0} ^ {0} = {1}", false, false ^ false);
-            Console.WriteLine("{0} ^ {1} = {2}", true, false, true ^ false);
-
-            Console.WriteLine("");
-
-            Console.WriteLine("Disyunción Inclusiva");
-            Console.WriteLine("{0} | {0} = {1}", true, true | true);
-            Console.WriteLine("{0} | {1} = {2}", true, false, true | false);
-            Console.WriteLine("{0} | {1} = {2}", false, false, false | false);
+            Console.WriteLine(fecha.Subtract(fechaConHora).Days);
 
             Console.Read();
         }
