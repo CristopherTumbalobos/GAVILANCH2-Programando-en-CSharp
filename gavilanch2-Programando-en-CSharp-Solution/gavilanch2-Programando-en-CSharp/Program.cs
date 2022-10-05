ea5288ad9c1cdc65e71f632f8aae1f82349ec7fd
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-05 17:08:56 -0500

    Fundamentos de C# | Declarando e inicializando variables

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -4,12 +4,24 @@ namespace gavilanch2_Programando_en_CSharp
 {
     class Program
     {
+        //BLOQUE: El interior entre dos llaves {}
         static void Main(string[] args)
         {
-            //Escribe hola mundo en la consola
-            Console.WriteLine("Hola mundo!");
-            //Nos da tiempo a ver el mensaje
-            Console.Read();
+            //Ejemplos de declaracion de una variable
+            int mes;
+            string nombre;
+            DateTime fechaEntrada;
+            bool procesoTerminado;
+
+            //Ejemplos de inicialización de una variable
+            mes = 7;
+            nombre = "Felipe";
+            procesoTerminado = true;
+            procesoTerminado = false;
+            
+            //Podemos inicializar y declara en una misma linea
+            string direccion = "Calle 123";
+            string direccionNueva= "Nueva Calle";
         }
     }
 }
