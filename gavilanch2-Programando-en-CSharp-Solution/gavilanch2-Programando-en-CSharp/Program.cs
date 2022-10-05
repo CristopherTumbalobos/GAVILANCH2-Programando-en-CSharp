7df8170661d8a4b156b2e585eeff555601703ee1
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-05 17:02:45 -0500

    Fundamentos de C# | Hola Mundo

---
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace gavilanch2_Programando_en_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribe hola mundo en la consola
+            Console.WriteLine("Hola mundo!");
+            //Nos da tiempo a ver el mensaje
+            Console.Read();
+        }
+    }
+}
