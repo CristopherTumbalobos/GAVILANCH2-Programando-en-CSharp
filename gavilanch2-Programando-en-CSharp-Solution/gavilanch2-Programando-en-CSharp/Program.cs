f8d250d99d06cdcc275858c69fd5640ce64ba062
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-05 17:32:32 -0500

    Fundamentos de C# | Tipos de datos

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -6,30 +6,17 @@ namespace gavilanch2_Programando_en_CSharp
     {
         static void Main(string[] args)
         {
-            PreguntaNombreYEdad();
-            Console.Read();
-        }
-
-        /// <summary>
-        /// Hacer un programa que le pregunte al usuario su nombre y edad, posterior a eso debe mostrar un mensaje en pantalla con el nombre del usuario y su edad.
-        /// Pista: Utilice Console.ReadLine para leer la información que ingresa el usuario
-        /// Pista 2: Utilice int.Parse para convertir de tipo string a tipo int
-        /// </summary>
-        /// <param name="args"></param>
-        static void PreguntaNombreYEdad()
-        {
-            string nombre;
-            int edad;
-
-            Console.Write("Ingrese su nombre: ");
-            nombre = Console.ReadLine();
-
-            Console.Write("Ingrese su edad: ");
-            edad = int.Parse(Console.ReadLine());
-
-            //FORMAT: Nos permite insertar valores en un string de una manera sencilla
-            string output = string.Format("Su nombre es {0} y tiene {1} años de edad", nombre, edad);
-            Console.WriteLine(output);
+            //Tipos de Datos
+            //TIPOS DE VALORES
+                //1.Tipos simples: int, byte, char, double, decimal, bool
+                //2. Enums
+                //3. Struct
+                //4. Nullables
+            //2. TIPOS DE REFERENCIA
+                //1. Class: object, string, class
+                //2. Interfaces
+                //3. Arrays
+                //4. Delegates
         }
     }
 }
