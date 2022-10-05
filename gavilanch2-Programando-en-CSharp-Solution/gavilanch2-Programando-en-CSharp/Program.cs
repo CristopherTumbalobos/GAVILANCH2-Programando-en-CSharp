b74bc8ea25f94d90d091fec2d24cb35bfc6c50d9
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-05 18:25:12 -0500

    Fundamentos de C# | Alcance y bloques

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -4,23 +4,41 @@ namespace gavilanch2_Programando_en_CSharp
 {
     class Program
     {
+        //ALCANCE: Desde donde podemos acceder a una variable (atributo) o metodo, o instanciar una clase
+        //BLOQUE: Interior entre dos llaves {}
         static void Main(string[] args)
         {
-            DateTime fecha = new DateTime(2014, 10, 23);        
-            DateTime fechaConHora = new DateTime(2005, 4, 23, 9, 30, 45);
 
-            Console.WriteLine(fecha.ToString());
-            Console.WriteLine(fecha.ToString("MM/dd/yyyy"));
-            Console.WriteLine(fecha.ToString("dd-MM-yy"));
-            Console.WriteLine(fechaConHora.ToString("dd*MM*yy hh:mm:ss"));
-            Console.WriteLine(fecha.AddDays(45).ToString());
+            string nombre = "Felipe";
 
-            Console.WriteLine(fechaConHora.DayOfWeek);
-            Console.WriteLine(fechaConHora.Date);
+            nombre = "Juan";
+            edad = 45; //Si se puede asignar valor a "edad" porque la variable "edad" es static
 
-            Console.WriteLine(fecha.Subtract(fechaConHora).Days);
+            var p = new Persona();
+            p.PaisOrigen = "Republica Dominicana";
+            //p.edad2 = 45; (ERROR: "edad2" es un atributo privado)
+            p.obtenerEdad();
 
             Console.Read();
+        }
+
+        public void Main2()
+        {
+            //nombre = "Pedro"; (ERROR: "nombre" no esta definida dentro del mismo bloque)
+        }
+
+        //nombre = "Pedro"; (ERROR: "nombre" no esta definida dentro del mismo bloque)
+        static int edad = 21;
+
+        class Persona
+        {
+            public string PaisOrigen;
+            private int edad2;
+
+            public int obtenerEdad()
+            {
+                return edad2;
+            }
         }
     }
 }
