17b3ff4b6360c5faf5aec01951e743eaadbe6ca7
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-13 21:07:37 -0500

    Fundamentos de C# | Condicional if

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -7,78 +7,44 @@ namespace gavilanch2_Programando_en_CSharp
         static void Main(string[] args)
         {
 
-            string nombre = "Felipe";
-            string nombreMayuscula = "FELIPE";
-
-            //Console.WriteLine(nombre.ToUpper() == nombreMayuscula.ToUpper());
-
-            ////Length: Calcula la longitud del string
-            //Console.WriteLine(nombre.Length);
-            //Console.WriteLine(frase.Length);
-
-            ////Substring: Obtener un string a partir de otro string
-            //Console.WriteLine(nombre.Substring(3));
-            //Console.WriteLine(frase.Substring(7, 4));
-
-            ////StartsWith: Para verificar si el string empieza con una subcadena determinada
-            //Console.WriteLine(frase.StartsWith("Juan"));
-            //Console.WriteLine(frase.StartsWith("Jose"));
-
-            ////EndsWith: Para verificar si el string termina con una subcadena en especifico
-            //Console.WriteLine(frase.EndsWith("amigos"));
-            //Console.WriteLine(frase.EndsWith("enemigos"));
-
-            ////Contains: Para verificar si una subcadena está contenida en un string
-            //Console.WriteLine(frase.Contains("Jose"));
-            //Console.WriteLine(frase.Contains("Pedro"));
-
+            string nombre = "Felipess";
+            string nombreMayuscula = "FELIPESS";
             string frase = "Juan y Jose son amigos";
 
-            ////IndexOf: A partir de cual se encuentra una determinada subcadena
-            //var indiceDeJose = frase.IndexOf("Jose");
-            //var indiceDePedro = frase.IndexOf("Pedro");
+            var longitudNombre = nombre.Length;
 
-            //-1: Valor cuando no existe o no se encuentra
+            Console.WriteLine(longitudNombre);
+            if (nombre.Length == 6)
+            {
+                Console.WriteLine(frase.Length);
+            }
+            else if (longitudNombre == 7)
+            {
+                Console.WriteLine("Longitud es 7");
+            }
+            else if (longitudNombre == 8)
+            {
+                Console.WriteLine("Longitud es 8");
+                Console.WriteLine("Aqui estamos");
+                Console.WriteLine("C# es cool");
+                ComparaNombres(nombre, nombreMayuscula);
 
-            //Console.WriteLine(indiceDeJose);
-            //Console.WriteLine(indiceDePedro);
+            }
+            else
+            {
+                Console.WriteLine(nombre == nombreMayuscula);
+            }
 
-            //operador + : Concatenar strings 
-            //Console.WriteLine(nombre + ", " + frase);
-
-            //ToLower: Colocar en minuscula 
-            //Console.WriteLine(frase.ToLower());
-
-            //ToUpper: Colocar en mayuscula
-            //Console.WriteLine(frase.ToUpper());
-
-            //Trim: Elimina los espacios vacios al inicio y al final
-            //string ejemploTrim = " Felipe ";
-            //Console.WriteLine(ejemploTrim);
-            //Console.WriteLine(ejemploTrim.Trim());
-            //Console.WriteLine(nombre == ejemploTrim);
-            //Console.WriteLine(nombre == ejemploTrim.Trim());
-
-            //Format: Sustituciones de subcadenas especiales dentro de un string
-            //string ejemploFormat = "Felicidades {0} en tu cumpleaños {1}! {0}";
-            //var resultadoFormat = string.Format(ejemploFormat, nombre, 50);
-            //Console.WriteLine(resultadoFormat);
-
-            //Replace: Reemplazos dentro de un string con sintaxis más amigable
-            string ejemploReplace = "Felicidades @nombre en tu cumpleaños número @edad! @nombre";
-
-            var resultadoReplace = ejemploReplace.Replace("@nombre", nombre);
-            resultadoReplace = resultadoReplace.Replace("@edad", "50");
-            Console.WriteLine(resultadoReplace);
-
-            //IsNullOrWhiteSpace: Verifica si un string es nulo o consiste solo en espacios en blanco
-            string stringEspaciosEnBlanco = "      ";
-            string stringNulo = null;
-            Console.WriteLine(string.IsNullOrWhiteSpace(stringEspaciosEnBlanco));
-            Console.WriteLine(string.IsNullOrWhiteSpace(stringNulo));
-            Console.WriteLine(string.IsNullOrWhiteSpace(nombre));
 
             Console.Read();
+        }
+
+        private static void ComparaNombres(string nombre, string nombreMayuscula)
+        {
+            if (nombre.ToUpper() == nombreMayuscula.ToUpper())
+            {
+                Console.WriteLine("Los nombres son iguales");
+            }
         }
     }
 }
