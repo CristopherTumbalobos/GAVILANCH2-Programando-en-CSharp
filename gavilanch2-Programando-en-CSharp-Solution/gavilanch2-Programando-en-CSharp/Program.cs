d5bd83a3e5c593b904ff849842a7ec25b64cd7e8
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-13 21:15:48 -0500

    Fundamentos de C# | Iterador foreach

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -7,16 +7,20 @@ namespace gavilanch2_Programando_en_CSharp
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i + 1);
-            }
 
             List<string> nombres = new List<string>() { "felipe", "maria", "leonardo", "josefina" };
 
-            for (int i = 0; i < nombres.Count; i++)
+            //for (int i = 0; i < nombres.Count; i++)
+            //{
+            //    nombres[i] = nombres[i] + "s";
+            //    Console.WriteLine(nombres[i]);
+            //}
+
+            //FOREACH: No puede editar los elementos de la lista que estas iterando, en cambio FOR si puede
+            foreach (string nombre in nombres)
             {
-                Console.WriteLine(nombres[i].ToUpper());
+                nombre = nombre + "s";
+                Console.WriteLine(nombre.ToUpper());
             }
 
             Console.Read();
