e6c381151b56a14e104e7cea394de18e5cb08e3b
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-15 18:09:51 -0500

    Fundamentos de C# | Strings Mágicos

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -3,30 +3,21 @@ using System.Collections.Generic;
 
 namespace gavilanch2_Programando_en_CSharp
 {
-    //ENUM: Para evitar los números mágicos (con significado implícito)
-    enum EstatusOperacion
-    {
-        Exitoso = 1,
-        ClienteNoEncontrado = 2,
-        ErrorDeSistema = 5,
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            int estatusOperacion = 5;
+            string estatusOperacion = "K15";
 
-            //(int): Casteandolo a int
-            if (estatusOperacion == (int)EstatusOperacion.Exitoso)
+            if (estatusOperacion == EstatusOperaciones._exitoso)
             {
                 // ...
             }
-            else if (estatusOperacion == (int)EstatusOperacion.ClienteNoEncontrado)
+            else if (estatusOperacion == EstatusOperaciones._clienteNoEncontrado)
             {
                 // ...
             }
-            else if (estatusOperacion == (int)EstatusOperacion.ErrorDeSistema)
+            else if (estatusOperacion == EstatusOperaciones._errorDelSistema)
             {
                 // ...
             }
@@ -34,23 +25,12 @@ namespace gavilanch2_Programando_en_CSharp
             Console.Read();
         }
 
-        private static void OtroMetodo()
+        //Por si los Estatus deben ser accesibles para cualquiera
+        public static class EstatusOperaciones
         {
-            int estatusOperacion = 5;
-
-            if (estatusOperacion == (int)EstatusOperacion.Exitoso)
-            {
-                // ...
-            }
-            else if (estatusOperacion == (int)EstatusOperacion.ClienteNoEncontrado)
-            {
-                // ...
-            }
-            else if (estatusOperacion == (int)EstatusOperacion.ErrorDeSistema)
-            {
-                // ...
-            }
+            public const string _exitoso = "K120";
+            public const string _clienteNoEncontrado = "P4";
+            public const string _errorDelSistema = "K15";
         }
-
     }
 }
