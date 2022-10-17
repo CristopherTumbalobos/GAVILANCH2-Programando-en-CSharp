1b40b5bfb6cb0e2357d1f80dad26d8c071822fcd
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-17 09:00:51 -0500

    Clases y objetos | Indexadores en C#

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -11,19 +11,20 @@ namespace gavilanch2_Programando_en_CSharp
             var v2 = new Vector(new List<int> { 1, 2 });
             Console.WriteLine(v2.Componentes.ToString());
             Console.WriteLine(v2.Dimension);
-            Console.WriteLine(v2.Nombre);
+            v2[0] = 5;
+            Console.WriteLine("Indexador");
+            Console.WriteLine(v2[0]);
+            Console.WriteLine(v2.Componentes[1]);
             Console.Read();
         }
     }
 
     class Vector
     {
-        //CAMPO: Variable declarada a nivel de clase
+        //CAMPO
         private List<int> _componentes;
 
-        //PROPIEDADES: Método especial que devuelve y asigna un valor
-        //GET: Obtener un valor
-        //SET: Asignar un valor
+        //PROPIEDADES
         public List<int> Componentes
         {
             get
@@ -32,10 +33,19 @@ namespace gavilanch2_Programando_en_CSharp
             }
         }
 
+        //INDEXADOR: Propiedad especial
+        public int this[int i]
+        {
+            get { return _componentes[i]; }
+            set { _componentes[i] = value; }
+        }
+        //value: Representa el valor que le estamos asignando a una propiedad
+
         public int Dimension { get { return _componentes.Count; } }
 
         public string Nombre { get; private set; }
 
+        
         //CONSTRUCTOR
         public Vector(List<int> componentes)
         {
