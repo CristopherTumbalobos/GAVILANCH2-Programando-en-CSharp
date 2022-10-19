60148f0afe1555eb1ecff0616e92b3f51b905161
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-19 18:47:04 -0500

    Clases y Objetos | Encapsulamiento C#

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using ClassLibrary1;
 using Operaciones;
 
 namespace gavilanch2_Programando_en_CSharp
@@ -9,9 +8,62 @@ namespace gavilanch2_Programando_en_CSharp
     {
         static void Main(string[] args)
         {
-            Vectorcillo v = new Vectorcillo();
-            Console.WriteLine(v.Name);
+            var iteradordeLista = new IteradorDeLista();
+            iteradordeLista.Lista = new List<int>() { 1, 2, 3 };
+            iteradordeLista.EscribirLista();
+
+            iteradordeLista.Lista = null;
+            iteradordeLista.EscribirLista();
+
             Console.Read();
+        }
+    }
+
+    class IteradorDeLista
+    {
+        //Constructor que inicializa un campo
+        public IteradorDeLista()
+        {
+            _lista = new List<int>();
+        }
+
+        //Campo privado
+        private List<int> _lista;
+
+        //Encapsulamiento
+        //Permite manejar la recuperacion y escritura de variables o campos privados
+        //Propiedad: Nos permite encapsular nuestros campos
+        public List<int> Lista
+        {
+            get
+            { return _lista; }
+            set
+            {
+                if (value != null)
+                {
+                    _lista = value;
+                }
+                else
+                {
+                    Lista = new List<int>();
+                }
+            }
+        }
+
+        public void EscribirLista()
+        {
+            foreach (var item in _lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void EscribirLista2()
+        {
+            foreach (var item in _lista)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
