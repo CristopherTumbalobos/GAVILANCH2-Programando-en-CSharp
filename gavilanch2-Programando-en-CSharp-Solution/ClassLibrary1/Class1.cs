905400a486a970954142cc61e7701e9544eae34d
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-19 18:35:06 -0500

    Clases y Objetos | Agregando varios proyectos a la solución

---
+++ gavilanch2-Programando-en-CSharp-Solution/ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Vectorcillo
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ClassLibrary1;
 using Operaciones;
 
 namespace gavilanch2_Programando_en_CSharp
@@ -8,58 +9,9 @@ namespace gavilanch2_Programando_en_CSharp
     {
         static void Main(string[] args)
         {
-            SumaDesdeProgram(1, 3); //SumaDesdeProgram no es static
-
-            var p = new Program();
-            p.SumaDesdeProgram(1, 2);
-
-            Program.SumaDesdeProgram(1, 3); //SumaDesdeProgram no es static
-
-            Matematicas.SumaDesdeMatematicas(1, 3);
-
-            var factura = new Factura();
-            factura.SumaDesdeFactura(1, 2);
-            Factura.SumaDesdeFactura(1, 2);
-
-            //Cuando SI es static podemos acceder desde la clase.metodo, no podemos instanciarla
-            //Cuando NO es static podemos acceder a partir de un instancia
-
-        }
-        public int SumaDesdeProgram(int sumando1, int sumando2)
-        {
-            return sumando1 + sumando2;
-        }
-    }
-
-    static class Matematicas
-    {
-        const double PI = 3.1415;
-        public static int SumaDesdeMatematicas (int sumando1, int sumando2)
-        {
-            return sumando1 + sumando2;
-        }
-
-        public static double AreaDelCirculo(int radio)
-        {
-            return radio * radio * PI;
-        }
-    }
-
-    class Factura //Decidimos que no sea estatica porque requiere almacenar data, para cada factura
-    {
-        public int Id { get; set; }
-        public decimal Monto { get; set; }
-        public static int SumaDesdeFactura(int sumando1, int sumando2)
-        {
-            return sumando1 + sumando2;
-        }
-    }
-
-    class FacturaRepositorio
-    {
-        public void GuardarEnBaseDeDatos(Factura factura)
-        {
-            // Codigo para guardar la factura en base de datos
+            Vectorcillo v = new Vectorcillo();
+            Console.WriteLine(v.Name);
+            Console.Read();
         }
     }
 }
