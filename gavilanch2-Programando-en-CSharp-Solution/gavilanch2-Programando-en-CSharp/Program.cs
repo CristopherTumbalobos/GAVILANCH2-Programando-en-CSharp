cde7be65456e546e52b1778760b70e65fef75c13
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-19 09:36:20 -0500

    Clases y Objetos | Sobrecarga en C#

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -8,83 +8,40 @@ namespace gavilanch2_Programando_en_CSharp
     {
         static void Main(string[] args)
         {
-            var v1 = new Vector(new List<int> { 3, 4, 5 });
-            var v2 = new Vector(new List<int> { 1, 2, -9 });
-            Vector vectorSuma = v1.Suma(v2);
-            Vector vectorSuma2 = v1 + v2;
-            v1++;
+            double numero1 = 5;
+            double numero2 = 7;
+            double numero3 = 10;
+            double promedio = CalcularPromedio(numero1, numero2, numero3);
+
+
             Console.Read();
-
-        }
-    }
-
-    //CLASS: Tipo de referencia que podemos definir
-    //STRUCT: Tipo de valor que podemos definir, la usamos cuando nuestras instancias tengan un corto tiempo de vida, tiene poca data, problemas de performace cuando se castea muchas veces, se manejan mejor un arreglo de tipo de valor que de tipo de referencia.
-
-
-    struct Vector
-    {
-        //CAMPO
-        private List<int> _componentes;
-
-        //PROPIEDADES
-        public List<int> Componentes
-        {
-            get
-            {
-                return _componentes;
-            }
         }
 
-        //INDEXADOR
-        public int this[int i]
-        {
-            get { return _componentes[i]; }
-            set { _componentes[i] = value; }
-        }
+        //Sobrecarga: Varios métodos pueden tener el mismo nombre siempre y cuando sus valores de entrada sean diferentes
 
-        public int Dimension { get { return _componentes.Count; } }
-
-        public string Nombre { get; private set; }
-
-        //CONSTRUCTOR
-        public Vector(List<int> componentes)
-        {
-            _componentes = componentes;
-        }
-
-        //METODO 2
-        public static Vector operator +(Vector vector1, Vector vector2)
-        {
-            return vector1.Suma(vector2);
-        }
-
-        public static int operator *(Vector vector1, Vector vector2)
+        private static int CalcularPromedio(double numero1, double numero2, double numero3)
         {
             throw new NotImplementedException();
         }
 
-        public static Vector operator ++(Vector vector1)
+        private static double CalcularPromedio(double numero1, double numero2, double numero3)
         {
             throw new NotImplementedException();
         }
 
-        //METODO 1
-        public Vector Suma(Vector v2)
+        //private static double CalcularPromedio(int numero1, int numero2)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        private static double CalcularPromedio(int numero1, int numero2)
         {
-            if (Dimension != v2.Dimension)
-            {
-                throw new ApplicationException("Las dimensiones no son iguales");
-            }
+            return (numero1 + numero2) / 2.0;
+        }
 
-            List<int> resultado = new List<int>();
-
-            for (int i = 0; i < Dimension; i++)
-            {
-                resultado.Add(this[i] + v2[i]);
-            }
-
-            return new Vector(resultado);
+        private static double CalcularPromedio(double numero1, double numero2)
+        {
+            return (numero1 + numero2) / 2.0;
         }
     }
 }
