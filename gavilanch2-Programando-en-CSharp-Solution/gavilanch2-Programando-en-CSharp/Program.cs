97d01d263ff634d34a6fb6a36e9a0f4e4a91e437
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-19 10:06:36 -0500

    Clases y Objetos | Métodos de extensión en C#

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -8,41 +8,26 @@ namespace gavilanch2_Programando_en_CSharp
     {
         static void Main(string[] args)
         {
-            //OBJETO 1
-            var e1 = new Empresa();
-            e1._NombreLegal = "Primera Empresa";
-            e1._Direccion = "Primera Direccion";
+            5.ElevadoALa(3);
 
-            //OBJETO 2
-            var e2 = new Empresa();
-            e2._NombreLegal = "Segunda Empresa";
-            e2._Direccion = "Segunda Direccion";
-
-            //Contexto.metodo1(Objeto);
-            e1.metodo1(e2);
-            e2.metodo1(e1);
-            e1.metodo1(e1);
+            Console.WriteLine(5.ElevadoALa(3));
+            Console.WriteLine(7.ElevadoALa(2));
+            Console.WriteLine(15.Doble());
 
             Console.Read();
         }
+    }
 
-        class Empresa
+    public static class IntegerExtensionMethods
+    {
+        public static double ElevadoALa(this int valor, int exponente)
         {
-            //Campos
-            public string _NombreLegal;
-            public string _Direccion;
+            return Math.Pow(valor, exponente);
+        }
 
-            //Metodo
-            public void metodo1(Empresa empresa2)
-            {
-                //this: Sirve para indicar que estamos trabajando con miembros de la clase en el contexto que estamos.
-                var this_nombreLegal = this._NombreLegal;
-                var this_direccion = this._Direccion;
-                var miNombreLegal = _NombreLegal;//Igual a this._NombreLegal
-                var miDireccion = _Direccion;//Igual a this._Direccion
-                var empresa2_nombreLegal = empresa2._NombreLegal;
-                var empresa2_direccion = empresa2._Direccion;
-            }
+        public static double Doble(this int valor)
+        {
+            return valor * 2;
         }
     }
 }
