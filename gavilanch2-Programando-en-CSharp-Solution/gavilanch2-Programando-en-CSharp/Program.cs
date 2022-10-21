0ee88f1a9f0e8b6e2f88934c3198328fbbdb0058
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-20 20:45:34 -0500

    Clases y Objetos | Inyección de dependencia

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -8,9 +8,69 @@ namespace gavilanch2_Programando_en_CSharp
     {
         static void Main(string[] args)
         {
-            //PRAGMATISMO: Hacer solo cosas que te aporten
-            //Patron de diseño: Solución general reutilizable a un problema recurrente en un contexto determinado.
+            //Inyeccion de dependencia: Sirve para desacoplar nuestras clases y ahorrar codigo. Nos dá flexibilidad a la hora de escoger las dependencia de nuestra clase
+
+            //INYECCION DE DEPENDENCIA POR CONSTRUCTOR
+
+            var enviadorSMS = new EnviarMiniMensaje();
+            var enviadorMensaje = new EnviadorMensaje(enviadorSMS);//(enviadorSMS): Dependencia
+            enviadorMensaje.EnviarMensaje("un mensaje");
+
+            //INYECCION DE DEPENDENCIA MEDIANTE PROPIEDAD
+
+            //var enviadorSMS = new EnviarMiniMensaje();
+            //var enviadorMensaje = new EnviadorMensaje();
+            //enviadorMensaje.ImplementacionEnviadorMensaje = enviadorSMS;
+            //enviadorMensaje.EnviarMensaje("un mensaje");
+
             Console.Read();
+        }
+    }
+
+    public class EnviadorMensaje
+    {
+        //INYECCION DE DEPENDENCIA POR CONSTRUCTOR
+
+        public EnviadorMensaje(IEnviadorMensaje enviadorMensaje)
+        {
+            _enviadorMensaje = enviadorMensaje;
+        }
+
+        private IEnviadorMensaje _enviadorMensaje;
+
+        public void EnviarMensaje(string mensaje)
+        {
+            _enviadorMensaje.EnviarMensaje(mensaje);
+        }
+
+        //INYECCION DE DEPENDENCIA MEDIANTE PROPIEDAD
+
+        //public IEnviadorMensaje ImplementacionEnviadorMensaje { get; set; }
+
+        //private IEnviadorMensaje _enviadorMensaje;
+
+        //public void EnviarMensaje(string mensaje)
+        //{
+        //    ImplementacionEnviadorMensaje.EnviarMensaje(mensaje);
+        //}
+    }
+
+    public interface IEnviadorMensaje
+    {
+        void EnviarMensaje(string mensaje);
+    }
+    class EnviarMiniMensaje : IEnviadorMensaje
+    {
+        public void EnviarMensaje(string mensaje)
+        {
+            Console.WriteLine("Enviando minimensaje");
+        }
+    }
+    class EnviarCorreo : IEnviadorMensaje
+    {
+        public void EnviarMensaje(string mensaje)
+        {
+            Console.WriteLine("Enviando correo");
         }
     }
 }
