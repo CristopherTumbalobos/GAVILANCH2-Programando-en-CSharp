fa2e0b793bcac62c27f837a458c40390ddbe6f3b
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-10-20 20:16:13 -0500

    Clases y Objetos | Objeto Object y operador IS

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -8,47 +8,26 @@ namespace gavilanch2_Programando_en_CSharp
     {
         static void Main(string[] args)
         {
-            var miniMensaje = new EnviarMiniMensaje();
-            var enviarCorreo = new EnviarCorreo();
-            Enviar(miniMensaje, "prueba");
-            Enviar(enviarCorreo, "prueba2");
+            //Object: Todos los tipos de C# heredan de objeto
+            Object o = DateTime.Today;
+
+            if (o is DateTime)
+            {
+                //Casteando el objeto "o"
+                var fecha = (DateTime)o;
+            }
+            else if (o is string)
+            {
+                //Casteando el objeto "o"
+                var unString = (string)o;
+            }
 
             Console.Read();
         }
 
-        public static void Enviar(IEnviadorMensaje mensajero, string mensaje)
+        public static void ToStringObject(object obj)
         {
-            mensajero.EnviarMensaje(mensaje);
-        }
-    }
-
-    //Interface: Podemos usarlas para polimorfismo
-    interface IEnviadorMensaje
-    {
-        //Método dentro de la interfaz
-        void EnviarMensaje(string mensaje);
-    }
-
-    class EnviarMiniMensaje : IEnviadorMensaje
-    {
-        //Implementando el método de la interfaz
-        public void EnviarMensaje(string mensaje)
-        {
-            Console.WriteLine("Enviando minimensaje");
-        }
-        public string CualquierMetodo()
-        {
-            return "Lo que sea";
-        }
-    }
-
-    class EnviarCorreo : IEnviadorMensaje
-    {
-        //Implementando el método de la interfaz
-
-        public void EnviarMensaje(string mensaje)
-        {
-            Console.WriteLine("Enviando correo");
+            Console.WriteLine(obj.ToString());
         }
     }
 }
