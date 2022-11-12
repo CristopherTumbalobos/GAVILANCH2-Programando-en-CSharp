936222b9b5b1fee76955f5450fabf829e40d1e4c
Cristopher Tumbalobos <cristopher.tumbalobos@hotmail.com>
2022-11-11 21:44:46 -0500

    Clases y Objetos | Introducción a los genéricos C#

--- gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
+++ gavilanch2-Programando-en-CSharp-Solution/gavilanch2-Programando-en-CSharp/Program.cs
@@ -9,56 +9,39 @@ namespace gavilanch2_Programando_en_CSharp
 {
     class Program
     {
+        //Genéricos: Los genéricos nos sirven para parametrizar uno o varios tipos que usaremos dentro de un método, clase, interfaz, etc.
         static void Main(string[] args)
         {
-            var persona = new Persona() { Nombre = "Felipe" };
+            //Lista de tipo de valor
+            List<int> numeros = new List<int>() { 1, 2, 3, 4, 5 };
+            numeros.Add(6);
 
-            //Tipo de referencia
-            Console.WriteLine("--Tipo de referencia--");
-            Console.WriteLine("Previo al cambio");
-            Console.WriteLine(persona.Nombre);
-            EditarPersona(persona, "Roberto");
-            Console.WriteLine("Despues del cambio");
-            Console.WriteLine(persona.Nombre);
-
-            Console.WriteLine("");
-
-            //Tipo de valor
-            //Almacena directamente el valor
-            Console.WriteLine("--Tipo de valor--");
-            Console.WriteLine("Previo al cambio");
-            int edad = 9;
-            Console.WriteLine(edad);
-            //ref: Permite pasar un valor como una referencia
-            EditarEdad(ref edad, 14);
-            //EditarEdad(out edad, 14);
-            //OUT: Obliga asignarle un valor a las variables marcadas con out
-            //REF: No obliga declararle un valor a la variables marcadas con ref
-            //EditarEdad(out edad, 14);
-            Console.WriteLine("Despues del cambio");
-            Console.WriteLine(edad);
-
-            Console.Read();
+            //Lista de tipo de referencia
+            List<Persona> personas = new List<Persona>()
+            {
+                new Persona () {Nombre = "Felipe"},
+                new Persona () {Nombre = "Carla"}
+            };
         }
 
-        static void EditarPersona(Persona persona, string nuevoNombre)
+        private int Duplicar(int valor)
         {
-            persona.Nombre = nuevoNombre;
+            return valor * 2;
         }
 
-        static void EditarEdad(ref int edadActual, int nuevaEdad)
+        private double Duplicar(double valor)
         {
-            edadActual = nuevaEdad;
+            return valor * 2;
         }
-
-        //static void EditarEdad(out int edadActual, int nuevaEdad)
-        //{
-        //    edadActual = nuevaEdad;
-        //}
     }
 
     class Persona
     {
         public string Nombre { get; set; }
+    }
+
+    class Empresa
+    {
+        public string Direccion { get; set; }
     }
 }
